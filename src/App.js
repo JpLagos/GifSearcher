812e70bbcd76f7a5e51a136e8cbe27d58b2d1f40
Juan Lagos <juan.lagoshuerta@gmail.com>
2022-04-09 17:26:50 -0400

    First search changed to 'hello world'

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import GifGrid from './components/GifGrid'
 import React from 'react'
 
 const GifApp = () => {
-  const [categories, setCategories] = useState(['One Punch'])
+  const [categories, setCategories] = useState(['Hello world'])
 
   return (
     <div className='flex flex-col justify-center align-center text-center mx-auto'>
